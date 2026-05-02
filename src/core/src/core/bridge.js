34d382a72f61ae74b05ba7291325bc4c75c447b7
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-05-02 14:35:43 -0300

    Update bridge.js

--- src/core/src/core/bridge.js
+++ src/core/src/core/bridge.js
@@ -1,1 +1,136 @@
+import eventBus from "./eventBus";
+import pipeline from "./pipeline";
 
+
+const bridge = {
+
+  listeners:
+    [],
+
+
+  state: {
+
+    logs:
+      [],
+
+    result:
+      null,
+
+    running:
+      false
+  },
+
+
+  init(
+    onUpdate
+  ) {
+
+    // conecta UI ao eventBus
+
+    eventBus.on(
+      "log",
+      (
+        data
+      ) => {
+
+        this.state.logs.push(
+          data
+        );
+
+
+        if (
+          onUpdate
+        ) {
+
+          onUpdate(
+            this.state
+          );
+        }
+      }
+    );
+
+
+    this.listeners.push(
+      onUpdate
+    );
+  },
+
+
+  async run(
+    input,
+    onUpdate
+  ) {
+
+    this.state.running =
+      true;
+
+
+    this.state.logs =
+      [];
+
+
+    this.state.result =
+      null;
+
+
+    if (
+      onUpdate
+    ) {
+
+      onUpdate(
+        this.state
+      );
+    }
+
+
+    const result =
+      await pipeline.run(
+        input
+      );
+
+
+    this.state.result =
+      result;
+
+
+    this.state.running =
+      false;
+
+
+    if (
+      onUpdate
+    ) {
+
+      onUpdate(
+        this.state
+      );
+    }
+
+
+    return result;
+  },
+
+
+  getState() {
+
+    return this.state;
+  },
+
+
+  clear() {
+
+    this.state.logs =
+      [];
+
+    this.state.result =
+      null;
+
+    this.state.running =
+      false;
+  }
+
+};
+
+
+export default
+  bridge;
