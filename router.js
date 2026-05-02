882fe145491d8348c7fd959141bded149e02392f
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-05-02 10:57:35 -0300

    Update router.js

--- router.js
+++ router.js
@@ -1,1 +1,31 @@
+function route(parsed) {
 
+  if (parsed.createSite) {
+    return generateSite(parsed);
+  }
+
+  return "Comando não reconhecido";
+}
+
+function generateSite(p) {
+
+  let bg = p.dark ? "#0f1115" : "white";
+
+  return `
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    body { background: ${bg}; color: white; }
+  </style>
+</head>
+<body>
+
+<h1>Site Gerado</h1>
+
+${p.menu ? "<button>☰ Menu</button>" : ""}
+
+</body>
+</html>
+`;
+}
