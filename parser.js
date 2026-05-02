d2f249c5b0754fa68affdf69cfc6c72b3a51af6c
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-05-02 10:56:48 -0300

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,1 +1,10 @@
+function parseCommand(text) {
 
+  let cmd = text.toLowerCase();
+
+  return {
+    createSite: cmd.includes("site"),
+    dark: cmd.includes("dark"),
+    menu: cmd.includes("menu")
+  };
+}
