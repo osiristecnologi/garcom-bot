c5f518938a0e54abf591bdc3b4d309223d71f89c
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-05-02 14:17:55 -0300

    Update eventBus.js

--- src/core/eventBus.js
+++ src/core/eventBus.js
@@ -1,1 +1,73 @@
+class EventBus {
 
+  constructor() {
+
+    this.events = {};
+  }
+
+
+  on(
+    event,
+    callback
+  ) {
+
+    if (
+      !this.events[event]
+    ) {
+
+      this.events[event] =
+        [];
+    }
+
+
+    this.events[event].push(
+      callback
+    );
+  }
+
+
+  emit(
+    event,
+    data
+  ) {
+
+    const handlers =
+      this.events[event];
+
+
+    if (!handlers) return;
+
+
+    handlers.forEach(
+      cb => cb(data)
+    );
+  }
+
+
+  off(
+    event,
+    callback
+  ) {
+
+    if (
+      !this.events[event]
+    ) return;
+
+
+    this.events[event] =
+      this.events[event]
+        .filter(
+          cb =>
+            cb !== callback
+        );
+  }
+
+}
+
+
+const eventBus =
+  new EventBus();
+
+
+export default
+  eventBus;
