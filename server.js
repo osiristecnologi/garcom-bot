09399e42082ee88aa6022c32b46d4cb859a86ae9
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-05-02 11:53:51 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "Agent Studio Backend Online"
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
