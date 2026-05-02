4660f53bbaeea325b33bbeecc8fc3cc4361aade5
Osiris technolog <107819115+osiristecnologi@users.noreply.github.com>
2026-05-02 13:39:43 -0300

    Update index.js

--- src/tools/src/tools/index.js
+++ src/tools/src/tools/index.js
@@ -1,1 +1,51 @@
+import cryptoTool
+  from "./cryptoTool";
 
+import weatherTool
+  from "./weatherTool";
+
+import newsTool
+  from "./newsTool";
+
+import githubTool
+  from "./githubTool";
+
+import mapsTool
+  from "./mapsTool";
+
+import moviesTool
+  from "./moviesTool";
+
+import aiTool
+  from "./aiTool";
+
+
+
+const TOOLS = {
+
+  crypto:
+    cryptoTool,
+
+  weather:
+    weatherTool,
+
+  news:
+    newsTool,
+
+  github:
+    githubTool,
+
+  maps:
+    mapsTool,
+
+  movies:
+    moviesTool,
+
+  ai:
+    aiTool
+};
+
+
+
+export default
+  TOOLS;
