5b1b0599ccd694895b31d6bc3812b33eb955dd4f
Osiris technology <107819115+osiristecnologi@users.noreply.github.com>
2026-05-07 22:45:50 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,11 @@
-
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
