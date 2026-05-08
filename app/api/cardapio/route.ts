fb83679605df986b0435efc9ae3fd52e6bff7465
Osiris technology <107819115+osiristecnologi@users.noreply.github.com>
2026-05-08 00:36:58 -0300

    Update route.ts

--- app/api/cardapio/route.ts
+++ app/api/cardapio/route.ts
@@ -1,7 +1,11 @@
 import { NextResponse } from 'next/server'
 
+let cardapio = [
+  { id: 1, nome: "X-Burger", preco: 18.00, desc: "Pão, carne, queijo" },
+  { id: 2, nome: "X-Salada", preco: 20.00, desc: "Com alface e tomate" },
+  { id: 3, nome: "Coca 350ml", preco: 6.00, desc: "Lata gelada" }
+]
+
 export async function GET() {
-  return NextResponse.json({
-    cardapio: []
-  })
+  return NextResponse.json({ cardapio })
 }
