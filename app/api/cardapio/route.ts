2f8be0aea54c9c9ed0ca77ef55a3e88b9e9dc780
Osiris technology <107819115+osiristecnologi@users.noreply.github.com>
2026-05-07 23:27:28 -0300

    Update route.ts

--- app/api/cardapio/route.ts
+++ app/api/cardapio/route.ts
@@ -1,1 +1,15 @@
+import { NextResponse } from 'next/server'
 
+export async function GET() {
+  return NextResponse.json({ 
+    pedidos: [] 
+  })
+}
+
+export async function POST(request: Request) {
+  const body = await request.json()
+  return NextResponse.json({ 
+    message: "Pedido recebido", 
+    pedido: body 
+  })
+}
