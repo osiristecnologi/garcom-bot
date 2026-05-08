9c2926355039ba21bff727df157ab687a9d45b57
Osiris technology <107819115+osiristecnologi@users.noreply.github.com>
2026-05-07 22:56:39 -0300

    Add metadata and RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,16 @@
+export const metadata = {
+  title: 'Garcom Bot',
+  description: 'Bot do garçom',
+}
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
+}
